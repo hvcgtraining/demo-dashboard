2bf11083b3bb17a5600e0de07d2465dc41ba8649
hvcgtraining <hvcgtraining@gmail.com>
2019-06-13 10:32:25 +0700

    Add
    add

--- frw_hvcg/dist/js/dashboard.js
+++ frw_hvcg/dist/js/dashboard.js
@@ -6,15 +6,15 @@ clicka.addEventListener("click", function () {
   menu01.classList.toggle("setwidth");
   var menu01 = document.getElementById("toggleclicka");
   menu01.classList.toggle("setwidtha");
-});
-$(".nav-box").each(function (index, value) {
-  var nav = $(value);
-  var nav01 = nav.find(".dropdownmenu");
-  nav01.click(function () {
-    var navsub = nav.find(".sibar-menu-sub");
-    navsub.slideToggle('swing');
-  });
-}); // $(".nav-box1").each(function(index,value){
+}); // $(".nav-box").each(function(index,value){
+//     var nav = $(value);
+//     var nav01 = nav.find(".dropdownmenu");
+//     nav01.click(function(){
+//         var navsub = nav.find(".sibar-menu-sub");
+//         navsub.slideToggle('swing')
+//     })
+// })
+// $(".nav-box1").each(function(index,value){
 //     var nav = $(value);
 //     var nav01 = nav.find(".dropdownmenu1");
 //     nav01.click(function(){
@@ -39,4 +39,18 @@ $(".nav-box1").click(function (e) {
 });
 $('body').click(function (e) {
   $(".nav-box1.active").removeClass('active').find('.notifi__box').slideUp('swing');
+});
+$(".nav-box").click(function (e) {
+  e.stopPropagation(); // Ngan chan su kien click lan toa ra thang cha
+  // Ẩn het toan bo nhung thang khac
+
+  var navsub = $(this).find(".sibar-menu-sub");
+
+  if ($(this).hasClass('active') == false) {
+    $(".nav-box.active").removeClass('active').find('.sibar-menu-sub').slideUp('fast');
+    $(this).addClass('active');
+    navsub.slideDown('swing');
+  } else {
+    $(".nav-box.active").removeClass('active').find('.sibar-menu-sub').slideUp('fast');
+  }
 });
--- frw_hvcg/src/js/dashboard.js
+++ frw_hvcg/src/js/dashboard.js
@@ -5,14 +5,14 @@ var clicka = document.getElementById("toggle-menu");
         var menu01 = document.getElementById("toggleclicka");
         menu01.classList.toggle("setwidtha");
     });
-    $(".nav-box").each(function(index,value){
-        var nav = $(value);
-        var nav01 = nav.find(".dropdownmenu");
-        nav01.click(function(){
-            var navsub = nav.find(".sibar-menu-sub");
-            navsub.slideToggle('swing')
-        })
-    })
+    // $(".nav-box").each(function(index,value){
+    //     var nav = $(value);
+    //     var nav01 = nav.find(".dropdownmenu");
+    //     nav01.click(function(){
+    //         var navsub = nav.find(".sibar-menu-sub");
+    //         navsub.slideToggle('swing')
+    //     })
+    // })
     
     // $(".nav-box1").each(function(index,value){
     //     var nav = $(value);
@@ -41,6 +41,23 @@ var clicka = document.getElementById("toggle-menu");
         $(".nav-box1.active").removeClass('active').find('.notifi__box').slideUp('swing');
     })
 
+
+
+    $(".nav-box").click(function(e) {
+        e.stopPropagation(); // Ngan chan su kien click lan toa ra thang cha
+        // Ẩn het toan bo nhung thang khac
+        var navsub = $(this).find(".sibar-menu-sub");
+            
+        if($(this).hasClass('active') == false) {
+            $(".nav-box.active").removeClass('active').find('.sibar-menu-sub').slideUp('fast');
+            $(this).addClass('active');
+            navsub.slideDown('swing');
+        } else {
+            $(".nav-box.active").removeClass('active').find('.sibar-menu-sub').slideUp('fast');
+        }
+            
+        
+    })  
   
  
         
